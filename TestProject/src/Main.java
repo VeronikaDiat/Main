51a7972763fa09543dbe6bd14a330a7f4331cfda
Veronika <gav51@ukr.net>
2022-07-05 21:04:35 +0300

    second try

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Conflicting changes");
+        System.out.println("Conflicting changes - second try");
     }
 }
