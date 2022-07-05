440f606a372f301332661e7a874698bc93b494e9
Veronika <gav51@ukr.net>
2022-07-05 21:13:46 +0300

    Revert "second try"
    This reverts commit 51a7972763fa09543dbe6bd14a330a7f4331cfda.

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Conflicting changes - second try");
+        System.out.println("Conflicting changes");
     }
 }
