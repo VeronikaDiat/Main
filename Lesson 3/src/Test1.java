63747513af66d79ed19bd7eb244b981bc9fd5275
Veronika <gav51@ukr.net>
2022-07-16 19:33:01 +0300

    lesson 3

---
+++ Lesson 3/src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void Name()
+    {
+        String name = "Veronika";
+        System.out.println("My name is"+" "+name);
+        System.out.println("I love Java");
+    }
+}
