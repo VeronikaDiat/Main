3d1f68dbab5e91b0ef568f6fcb1bc64627226f8a
Veronika <gav51@ukr.net>
2022-07-18 22:22:27 +0300

    removed unused code

--- Lesson 3/src/Test1.java
+++
@@ -1,8 +0,0 @@
-public class Test1 {
-    public static void Name()
-    {
-        String name = "Veronika";
-        System.out.println("My name is"+" "+name);
-        System.out.println("I love Java");
-    }
-}
--- Lesson 3/src/Test9.java
+++
@@ -1,9 +0,0 @@
-public class Test9 {
-    public static void Loop2 (){
-        int i=10;
-        while (i>=0){
-            System.out.println("Number is:"+ i);
-            i--;
-        }
-    }
-}
